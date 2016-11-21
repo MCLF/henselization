9afaf46d1f8b90a89dec02a303ee4a8c8ff6835c
Julian Rüth <julian.rueth@fsfe.org>
2016-11-21 15:01:07 -0500

    Fix module loading for doctests

--- __init__.py
+++ __init__.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
+#*****************************************************************************
+#       Copyright (C) 2016 Julian Rüth <julian.rueth@fsfe.org>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+# Fix doctests so they work in standalone mode (when invoked with sage -t, they run within the completion/ directory)
+import sys, os
+if hasattr(sys.modules['__main__'], 'DC') and 'standalone' in sys.modules['__main__'].DC.options.optional:
+    sys.path.append(os.path.dirname(os.getcwd()))
+
 from mac_lane import *
 
-from .completion import Completion, CompleteRing_base, CompleteDomain, CompleteField
-from .base_element import BaseElement
-from .mac_lane_element import MacLaneElement
+from completion import Completion, CompleteRing_base, CompleteDomain, CompleteField
+from base_element import BaseElement
+from mac_lane_element import MacLaneElement
 import valuation
-from .valuation import CompletionValuation
+from valuation import CompletionValuation
 
 
 from sage.structure.factory import register_factory_unpickle
