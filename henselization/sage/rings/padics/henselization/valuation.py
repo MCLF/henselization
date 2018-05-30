14cfe4470ca2e67db286ceddec67a583325a39f7
Julian Rüth <julian.rueth@fsfe.org>
2018-05-31 00:24:10 +0200

    Fix pickling issue

--- henselization/sage/rings/padics/henselization/valuation.py
+++ henselization/sage/rings/padics/henselization/valuation.py
@@ -65,7 +65,7 @@ class HenselizationValuationFactory(UniqueFactory):
         parent = DiscretePseudoValuationSpace(domain)
         return parent.__make_element_class__(HenselizationValuation)(parent)
 
-Valuation = HenselizationValuationFactory("henselization.valuation.Valuation")
+Valuation = HenselizationValuationFactory("sage.rings.padics.henselization.valuation.Valuation")
 
 
 class HenselizationValuation(DiscreteValuation):
