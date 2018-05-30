b2fd3571664283baa811f771342bba03e51522bc
Julian Rüth <julian.rueth@fsfe.org>
2018-05-30 23:52:05 +0200

    Use an unreleased version of recursive-monkey-patch
    to make things work for the time being

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="henselization",
     packages=find_packages(include=['henselization*']),
-    install_requires=['recursive-monkey-patch']
+    install_requires=['recursive-monkey-patch==0.3.0'],
+    dependency_links=['https://github.com/saraedum/recursive-monkey-patch/tarball/import#egg=recursive-monkey-patch-0.3.0']
 )
 
 
