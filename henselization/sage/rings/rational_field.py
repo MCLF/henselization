ad1d429b3ef22bdda1042961e7c3199305e00981
Julian Rüth <julian.rueth@fsfe.org>
2020-07-02 23:16:05 +0200

    add documentation for QQ.henselization

--- henselization/sage/rings/rational_field.py
+++ henselization/sage/rings/rational_field.py
@@ -1,4 +1,23 @@
 class RationalField:
     def henselization(self, prime):
+        r"""
+        Return the Henselization of the rationals at the prime number or ideal
+        ``prime``.
+
+        EXAMPLES::
+
+            sage: from henselization import *
+            sage: H = QQ.henselization(5)
+
+        ::
+
+            sage: R.<x> = QQ[]
+            sage: f = x^2 + 1
+            sage: f.is_irreducible()
+            True
+            sage: f.change_ring(H).is_irreducible()
+            False
+
+        """
         from sage.rings.padics.henselization.henselization import Henselization
         return Henselization(self, self.valuation(prime))
